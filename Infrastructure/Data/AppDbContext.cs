3e5840b3707418dbe1166472b4294a9ed776ecb4
Lucas <lucasfl14022005@gmail.com>
2026-03-25 21:24:20 -0300

    feat: configure Program.cs with EF Core, SQLite and Swagger

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace ExchangeLeadSystem.Infrastructure.Data
+﻿using ExchangeLeadSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeLeadSystem.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Lead> Leads { get; set; }
+        public DbSet<LeadNote> LeadNotes { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
